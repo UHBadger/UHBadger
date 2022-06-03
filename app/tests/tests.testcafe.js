ab19127824edf54893835d2b0b36bc1d53089cb5
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2022-06-02 21:07:52 -1000

    Revert "remove stuff's in testcafe."
    This reverts commit d24e538dd4f90eb983af5115f04d31df498bd60e.

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -1,10 +1,10 @@
-// import { Selector, t } from 'testcafe';
-import { manageDatabasePage, signOutPage } from './simple.page';
+import { Selector, t } from 'testcafe';
+import { addStuffPage, editStuffPage, listStuffPage, listStuffAdminPage, manageDatabasePage, signOutPage } from './simple.page';
 import { signInPage } from './signin.page';
 import { navBar } from './navbar.component';
 import { signUpPage } from './signup.page';
 import { landingPage } from './landing.page';
-// import { COMPONENT_IDS } from '../imports/ui/utilities/ComponentIDs';
+import { COMPONENT_IDS } from '../imports/ui/utilities/ComponentIDs';
 
 /* global fixture:false, test:false */
 
@@ -41,14 +41,14 @@ test('Test that user pages show up', async () => {
   await navBar.gotoSigninPage();
   await signInPage.signin(credentials.username, credentials.password);
   await navBar.isLoggedIn(credentials.username);
-  // await navBar.gotoAddStuffPage();
-  // await addStuffPage.isDisplayed();
-  // await navBar.gotoListStuffPage();
-  // await listStuffPage.isDisplayed();
+  await navBar.gotoAddStuffPage();
+  await addStuffPage.isDisplayed();
+  await navBar.gotoListStuffPage();
+  await listStuffPage.isDisplayed();
   // want to see if we can get to the editStuffPage
-  // const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
-  // await t.click(editLinks.nth(0));
-  // await editStuffPage.isDisplayed();
+  const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
+  await t.click(editLinks.nth(0));
+  await editStuffPage.isDisplayed();
   await navBar.logout();
   await signOutPage.isDisplayed();
 });
@@ -57,16 +57,16 @@ test('Test that admin pages show up', async () => {
   await navBar.gotoSigninPage();
   await signInPage.signin(adminCredentials.username, adminCredentials.password);
   await navBar.isLoggedIn(adminCredentials.username);
-  // await navBar.gotoAddStuffPage();
-  // await addStuffPage.isDisplayed();
-  // await navBar.gotoListStuffPage();
-  // await listStuffPage.isDisplayed();
+  await navBar.gotoAddStuffPage();
+  await addStuffPage.isDisplayed();
+  await navBar.gotoListStuffPage();
+  await listStuffPage.isDisplayed();
   // want to see if we can get to the editStuffPage
-  // const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
-  // await t.click(editLinks.nth(0));
-  // await editStuffPage.isDisplayed();
-  // await navBar.gotoListStuffAdminPage();
-  // await listStuffAdminPage.isDisplayed();
+  const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
+  await t.click(editLinks.nth(0));
+  await editStuffPage.isDisplayed();
+  await navBar.gotoListStuffAdminPage();
+  await listStuffAdminPage.isDisplayed();
   await navBar.gotoManageDatabasePage();
   await manageDatabasePage.isDisplayed();
 });
